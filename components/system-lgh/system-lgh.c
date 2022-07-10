f3a0e1ff6dec11ab89446ddbe02d90fcdf371360
vaca <wsmslgh@gmail.com>
2022-07-10 00:02:51 -0700

    asdfsdfgdsdvcgfgh

--- components/system-lgh/system-lgh.c
+++ components/system-lgh/system-lgh.c
@@ -1,8 +1,14 @@
 
+#include <esp_random.h>
 #include "system-lgh.h"
-int64_t low_system_timestamp_get(void){
+int64_t lc_low_system_timestamp_get(void){
     struct timeval tv;
     gettimeofday(&tv, NULL);
     return (tv.tv_sec * 1000LL + (tv.tv_usec / 1000LL));
 }
 
+
+
+uint32_t lc_low_system_random_number(void){
+    return esp_random();
+}
--- components/system-lgh/system-lgh.h
+++ components/system-lgh/system-lgh.h
@@ -5,5 +5,6 @@
 #ifndef BLE_SCAN_MODULE_SYSTEM_LGH_H
 #define BLE_SCAN_MODULE_SYSTEM_LGH_H
 #include <sys/time.h>
-int64_t low_system_timestamp_get(void);
+int64_t lc_low_system_timestamp_get(void);
+uint32_t lc_low_system_random_number(void);
 #endif //BLE_SCAN_MODULE_SYSTEM_LGH_H
